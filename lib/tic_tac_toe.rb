34d93617997f93589f6d01afa81d4b92cfc7e8d0
Shravani PL <pl.shravani@gmail.com>
2018-06-14 04:08:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,35 +1,39 @@
 class TicTacToe
-  
-  def initialize()
-    @board = [" "," "," "," "," "," "," "," "," "]
-  end
-  
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
-  
-  def display_board(board)
-   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-   puts "-----------"
-   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-   puts "-----------"
-   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
-  
-  def input_to_index(input)
-    input.to_i- 1 
-  end
-  
-  def move(index,pos = 'X')
-    @board[index] = pos
-  end 
-  
-  def position_taken?(index)
-    @board[index]!=" "
-    
-  def valid_move?(index)
-    !(position_taken(index) && index.between?(0,8))
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
-def turn_count
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, token)
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    @board[index] != " "
+  end
+
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
+  end
+
+  def turn_count
     @board.count{|square| square != " " }
   end
 
@@ -78,8 +82,6 @@ def turn_count
 
   def play
     turn until over?
-    puts winner ? "Congratulations #{winner}!" : "Its a draw!"
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end
-
-end 
-  
+end
